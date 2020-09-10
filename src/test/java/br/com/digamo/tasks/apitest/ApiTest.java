206d0c7cdec23f05cd05ab796c83dc8f3ede5082
digamo <digamo@gmail.com>
2020-09-09 23:31:38 -0300

    Testes de Integração do Tasks Api

---
+++ src/test/java/br/com/digamo/tasks/apitest/ApiTest.java
@@ -0,0 +1,88 @@
+package br.com.digamo.tasks.apitest;
+
+import java.util.Arrays;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+public class ApiTest {
+
+	@BeforeClass
+	public static void setup() {
+		RestAssured.baseURI = "http://localhost:8001/tasks-api";
+	}
+	
+	@Test
+	public void shouldReturnTasks() {
+		RestAssured.given()
+			.when()
+				.get("/todo")
+			.then()
+				.statusCode(200)
+			;
+	}
+	
+	@Test
+	public void shouldSaveTaskSuccessfully() {
+		RestAssured.given()
+				.body("{ \"name\": \"Task Test\", \"dueDate\": \"2030-01-01\" }")
+				.contentType(ContentType.JSON)
+			.when()
+				.post("/todo")
+			.then()
+				.statusCode(201)
+			;
+	}
+
+	@Test
+	public void shouldNotSaveTaskWithEmptyName() {
+		RestAssured.given()
+				.body("{ \"name\": \"\", \"dueDate\": \"2030-01-01\" }")
+				.contentType(ContentType.JSON)
+			.when()
+				.post("/todo")
+			.then()
+				.statusCode(400)
+				.body("message", CoreMatchers.equalTo(Arrays.asList("Fill the task description.")))
+				
+			;
+	}
+
+	@Test
+	public void shouldNotSaveTaskWithEmptyDate() {
+		RestAssured.given()
+				.body("{ \"name\": \"Task Test\", \"dueDate\": \"\" }")
+				.contentType(ContentType.JSON)
+			.when()
+				.post("/todo")
+			.then()
+				.statusCode(400)
+				.body("message", CoreMatchers.equalTo(Arrays.asList("Fill the due date.")))
+				
+			;
+	}
+
+	@Test
+	public void shouldNotSaveTaskWithDateLast() {
+		RestAssured.given()
+				.body("{ \"name\": \"Task Test\", \"dueDate\": \"2010-01-01\" }")
+				.contentType(ContentType.JSON)
+			.when()
+				.post("/todo")
+			.then()
+				.statusCode(400)
+				.body("message", CoreMatchers.equalTo(Arrays.asList("Due date must not be in past.")))
+				
+			;
+	}
+
+
+}
+
+
+ 
+
